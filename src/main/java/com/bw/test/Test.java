eec22815f3ff0ad2fc62654b086c2928bed83de8
BW05Genius <1061668182@qq.com>
2019-09-05 08:11:28 +0800

    初代版本

---
+++ src/main/java/com/bw/test/Test.java
@@ -0,0 +1,10 @@
+package com.bw.test;
+/** 
+* @author 作者:王澍
+* @version 创建时间：2019年9月5日 上午8:08:37  
+*/
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+	}
+}
